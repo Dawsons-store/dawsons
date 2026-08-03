6a685a284d21f7f443846e2d6293501f61398aaf
Nadiajabbar <jabbarnadia11@gmail.com>
2026-08-03 03:39:45 -0700

    Fixed mobile menu

--- js/script.js
+++ js/script.js
@@ -81,3 +81,13 @@ hero.style.backgroundImage =
 }, 4000);
 
 }
+// ================ MOBILE MENU ================
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuBtn && navLinks) {
+    menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
